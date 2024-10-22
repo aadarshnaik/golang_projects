3d5c96008976b038cc35508b98b4216c4a18b78a
aadarshnaik <aadarshnaik6@gmail.com>
2024-10-22 21:57:44 +0530

    go-postgres without gorm

---
+++ go-postgres/go.mod
@@ -0,0 +1,9 @@
+module github.com/aadarshnaik/golang_projects/go-postgres
+
+go 1.22.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
---
+++ go-postgres/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/aadarshnaik/golang_projects/go-postgres/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/stock", middleware.GetAllStock).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deletestock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	return router
+}
